f51b3106e9cc655ab5e7828e430a7098b14f21ea
ruimpz <ruimpz@gmail.com>
2019-02-09 20:23:58 +0000

    trasition to pygame

--- 2048.py
+++ 2048.py
@@ -12,10 +12,17 @@ class Board():
 
 
     def draw(self):
-        print()
+#        print()
+#        for i in range(self.board_size):
+#            print(self.board[i])
+#        print()
         for i in range(self.board_size):
-            print(self.board[i])
-        print()
+            for j in range(self.board_size):
+                x_pos = j * 200
+                y_pos = i * 200
+                color = colors[self.board[i][j]]
+                pygame.draw.rect(screen, color, [x_pos, y_pos, 200, 200])
+
 
 
     def get_piece(self):
@@ -76,21 +83,75 @@ class Board():
         self.board = t.transpose()
 
 
+clock = pygame.time.Clock()
+tick_rate = 30
+game_over = False
+
+colors = {
+    0: (60, 56, 54),
+    2: (235, 219, 178),
+    4: (251, 241, 199),
+    8: (69, 133, 136),
+    16: (142, 192, 124),
+    32: (184, 187, 28),
+    64: (250, 189, 47),
+    128: (211, 134, 155),
+    256: (251, 73, 52),
+    512: 0,
+    1024: 0,
+    2048: 0
+}
+
+screen = pygame.display.set_mode((800,800))
 
 board = Board(4)
 board.get_piece()
 
-while True:
-    board.get_piece()
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+   
     board.draw()
-    move = input()
-    if move == "a":
-        board.move_left()
-    if move == "s":
-        board.move_down()
-    if move == "d":
-        board.move_right()
-    if move == "w":
-        board.move_up()
+    pygame.display.update()
+#    clock.tick(tick_rate)
+    
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            board.move_left()
+            board.get_piece()
+        elif event.key == pygame.K_RIGHT:
+            board.move_right()
+            board.get_piece()
+        elif event.key == pygame.K_UP:
+            board.move_up()
+            board.get_piece()
+        elif event.key == pygame.K_DOWN:
+            board.move_down()
+            board.get_piece()
+    
+    pygame.time.delay(80)
+pygame.quit()
+quit()
+
+
+
+
+
+
+#while True:
+#    board.get_piece()
+#    board.draw()
+#
+#    move = input()
+#    if move == "a":
+#        board.move_left()
+#    if move == "s":
+#        board.move_down()
+#    if move == "d":
+#        board.move_right()
+#    if move == "w":
+#        board.move_up()
 
 
